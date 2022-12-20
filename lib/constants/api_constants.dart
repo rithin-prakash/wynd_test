556938e6d2565218c0d92fe7ed823757a28535dd
rithin <rithinprakash@gmail.com>
2022-12-20 23:10:43 +0530

    api constants

---
+++ lib/constants/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static const baseUrl = 'https://jsonplaceholder.typicode.com/';
+
+  static const postList = 'posts';
+  static const postDetail = 'posts/';
+  static const postComment = '/comments';
+}
