4e0f6c5bc0ba099a4ab9dd7a0f4298a36096d23d
rithin <rithinprakash@gmail.com>
2022-12-21 22:59:41 +0530

    main nav setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'package:wynd_test/constants/route_names.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:wynd_test/blocs/comment_bloc/comment_bloc.dart';
+import 'package:wynd_test/blocs/post_bloc/post_cubit.dart';
+import 'package:wynd_test/blocs/post_detail_bloc/post_detail_cubit.dart';
 import 'package:wynd_test/core/route_generator.dart';
+import 'package:wynd_test/screens/splashscreen/splashscreen_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +17,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      onGenerateRoute: RouteGenerator.generateRoute,
-      initialRoute: RouteName.home,
-    );
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => PostCubit()..loadPostList()),
+          BlocProvider(create: (_) => PostDetailCubit()),
+          BlocProvider(create: (_) => CommentCubit())
+        ],
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.amber,
+          ),
+          onGenerateRoute: RouteGenerator.generateRoute,
+          // initialRoute: RouteName.home,
+          home: SplashscreenPage(),
+        ));
   }
 }
